2ef20fff93d616381f7caf82a423ab7be0205f16
NicoleBarra <38593156+NicoleBarra@users.noreply.github.com>
2019-04-12 18:09:54 -0500

    hiii

---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        gameObject.transform.Translate(new Vector3(horizontal * speed, 0, 0));
+    }
+}
